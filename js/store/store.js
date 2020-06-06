ff7be4929363b460eb90379f6737c938d47f07c0
dean <deanlloyd64@gmail.com>
2020-06-06 17:47:23 -0400

    add Proxy to store, add 'set' and 'get' traps to monitor times the object is asked for data and changes to object

--- js/store/store.js
+++ js/store/store.js
@@ -18,6 +18,24 @@ export default class Store {
             self.mutations = params.mutations;
         }
         
+        self.state = new Proxy((params.state || {}), {
+            set: function(state, key, value){
+                state[key] = value;
+
+                console.log(`stateChange: ${key}: ${value}`);
+
+                self.events.publish('stateChange', self.state);
+
+                if(self.status !== 'mutation'){
+                    console.warn(`You should use a mutation to set ${key}`);
+                }
+
+                self.status = 'resting;'
+
+                return true;
+            }
+        });
+        
     }
 
 }
