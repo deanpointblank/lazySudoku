e936b6f2c5bda58cb79598dff8f447ec8a4bc98f
dean <deanlloyd64@gmail.com>
2020-06-11 20:33:18 -0400

    create basic board layout in divs

--- src/Containers/gameBoard.js
+++ src/Containers/gameBoard.js
@@ -10,8 +10,168 @@ export default class GameBoard extends Component{
     
     render(){
         return(
-            <section>
-                <p>This is where the game goes</p>
+            <section className="mainBoard">
+                <div className="gameRow">
+                    <div className="gameCellBlock">
+                        <div className="gameRow">
+                            <div className="gameCellRow">
+                                <div className="gameCell">1</div>
+                                <div className="gameCell">2</div>
+                                <div className="gameCell">3</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">4</div>
+                                <div className="gameCell">5</div>
+                                <div className="gameCell">6</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">7</div>
+                                <div className="gameCell">8</div>
+                                <div className="gameCell">9</div>
+                            </div>
+                        </div>
+                        <div className="gameRow">
+                            <div className="gameCellRow">
+                                <div className="gameCell">1</div>
+                                <div className="gameCell">2</div>
+                                <div className="gameCell">3</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">4</div>
+                                <div className="gameCell">5</div>
+                                <div className="gameCell">6</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">7</div>
+                                <div className="gameCell">8</div>
+                                <div className="gameCell">9</div>
+                            </div>
+                        </div>
+                        <div className="gameRow">
+                            <div className="gameCellRow">
+                                <div className="gameCell">1</div>
+                                <div className="gameCell">2</div>
+                                <div className="gameCell">3</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">4</div>
+                                <div className="gameCell">5</div>
+                                <div className="gameCell">6</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">7</div>
+                                <div className="gameCell">8</div>
+                                <div className="gameCell">9</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="gameCellBlock">
+                        <div className="gameRow">
+                            <div className="gameCellRow">
+                                <div className="gameCell">1</div>
+                                <div className="gameCell">2</div>
+                                <div className="gameCell">3</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">4</div>
+                                <div className="gameCell">5</div>
+                                <div className="gameCell">6</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">7</div>
+                                <div className="gameCell">8</div>
+                                <div className="gameCell">9</div>
+                            </div>
+                        </div>
+                        <div className="gameRow">
+                            <div className="gameCellRow">
+                                <div className="gameCell">1</div>
+                                <div className="gameCell">2</div>
+                                <div className="gameCell">3</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">4</div>
+                                <div className="gameCell">5</div>
+                                <div className="gameCell">6</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">7</div>
+                                <div className="gameCell">8</div>
+                                <div className="gameCell">9</div>
+                            </div>
+                        </div>
+                        <div className="gameRow">
+                            <div className="gameCellRow">
+                                <div className="gameCell">1</div>
+                                <div className="gameCell">2</div>
+                                <div className="gameCell">3</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">4</div>
+                                <div className="gameCell">5</div>
+                                <div className="gameCell">6</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">7</div>
+                                <div className="gameCell">8</div>
+                                <div className="gameCell">9</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="gameCellBlock">
+                        <div className="gameRow">
+                            <div className="gameCellRow">
+                                <div className="gameCell">1</div>
+                                <div className="gameCell">2</div>
+                                <div className="gameCell">3</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">4</div>
+                                <div className="gameCell">5</div>
+                                <div className="gameCell">6</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">7</div>
+                                <div className="gameCell">8</div>
+                                <div className="gameCell">9</div>
+                            </div>
+                        </div>
+                        <div className="gameRow">
+                            <div className="gameCellRow">
+                                <div className="gameCell">1</div>
+                                <div className="gameCell">2</div>
+                                <div className="gameCell">3</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">4</div>
+                                <div className="gameCell">5</div>
+                                <div className="gameCell">6</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">7</div>
+                                <div className="gameCell">8</div>
+                                <div className="gameCell">9</div>
+                            </div>
+                        </div>
+                        <div className="gameRow">
+                            <div className="gameCellRow">
+                                <div className="gameCell">1</div>
+                                <div className="gameCell">2</div>
+                                <div className="gameCell">3</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">4</div>
+                                <div className="gameCell">5</div>
+                                <div className="gameCell">6</div>
+                            </div>
+                            <div className="gameCellRow">
+                                <div className="gameCell">7</div>
+                                <div className="gameCell">8</div>
+                                <div className="gameCell">9</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         )
     }
