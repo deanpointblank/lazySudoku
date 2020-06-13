73a48f2cf93aa22e4eac1bc650a06b81743fd90d
dean <deanlloyd64@gmail.com>
2020-06-13 10:41:01 -0400

    extrapolate game row to component

--- src/Components/GameRow.js
+++ src/Components/GameRow.js
@@ -2,6 +2,60 @@ import React, { Component } from 'react';
 
 export const GameRow = (props) => {
     return(
-        
+        <div className="gameRow">
+            <div className="gameCellBlock">
+                <div className="gameRow">
+                    <div className="gameCellRow">
+                        <div className="gameCell">1</div>
+                        <div className="gameCell">2</div>
+                        <div className="gameCell">3</div>
+                    </div>
+                    <div className="gameCellRow">
+                        <div className="gameCell">4</div>
+                        <div className="gameCell">5</div>
+                        <div className="gameCell">6</div>
+                    </div>
+                    <div className="gameCellRow">
+                        <div className="gameCell">7</div>
+                        <div className="gameCell">8</div>
+                        <div className="gameCell">9</div>
+                    </div>
+                </div>
+                <div className="gameRow">
+                    <div className="gameCellRow">
+                        <div className="gameCell">1</div>
+                        <div className="gameCell">2</div>
+                        <div className="gameCell">3</div>
+                    </div>
+                    <div className="gameCellRow">
+                        <div className="gameCell">4</div>
+                        <div className="gameCell">5</div>
+                        <div className="gameCell">6</div>
+                    </div>
+                    <div className="gameCellRow">
+                        <div className="gameCell">7</div>
+                        <div className="gameCell">8</div>
+                        <div className="gameCell">9</div>
+                    </div>
+                </div>
+                <div className="gameRow">
+                    <div className="gameCellRow">
+                        <div className="gameCell">1</div>
+                        <div className="gameCell">2</div>
+                        <div className="gameCell">3</div>
+                    </div>
+                    <div className="gameCellRow">
+                        <div className="gameCell">4</div>
+                        <div className="gameCell">5</div>
+                        <div className="gameCell">6</div>
+                    </div>
+                    <div className="gameCellRow">
+                        <div className="gameCell">7</div>
+                        <div className="gameCell">8</div>
+                        <div className="gameCell">9</div>
+                    </div>
+                </div>
+            </div>
+        </div> 
     )
 }
--- src/Containers/gameBoard.js
+++ src/Containers/gameBoard.js
@@ -12,61 +12,7 @@ export default class GameBoard extends Component{
     render(){
         return(
             <section className="mainBoard">
-                <div className="gameRow">
-                    <div className="gameCellBlock">
-                        <div className="gameRow">
-                            <div className="gameCellRow">
-                                <div className="gameCell">1</div>
-                                <div className="gameCell">2</div>
-                                <div className="gameCell">3</div>
-                            </div>
-                            <div className="gameCellRow">
-                                <div className="gameCell">4</div>
-                                <div className="gameCell">5</div>
-                                <div className="gameCell">6</div>
-                            </div>
-                            <div className="gameCellRow">
-                                <div className="gameCell">7</div>
-                                <div className="gameCell">8</div>
-                                <div className="gameCell">9</div>
-                            </div>
-                        </div>
-                        <div className="gameRow">
-                            <div className="gameCellRow">
-                                <div className="gameCell">1</div>
-                                <div className="gameCell">2</div>
-                                <div className="gameCell">3</div>
-                            </div>
-                            <div className="gameCellRow">
-                                <div className="gameCell">4</div>
-                                <div className="gameCell">5</div>
-                                <div className="gameCell">6</div>
-                            </div>
-                            <div className="gameCellRow">
-                                <div className="gameCell">7</div>
-                                <div className="gameCell">8</div>
-                                <div className="gameCell">9</div>
-                            </div>
-                        </div>
-                        <div className="gameRow">
-                            <div className="gameCellRow">
-                                <div className="gameCell">1</div>
-                                <div className="gameCell">2</div>
-                                <div className="gameCell">3</div>
-                            </div>
-                            <div className="gameCellRow">
-                                <div className="gameCell">4</div>
-                                <div className="gameCell">5</div>
-                                <div className="gameCell">6</div>
-                            </div>
-                            <div className="gameCellRow">
-                                <div className="gameCell">7</div>
-                                <div className="gameCell">8</div>
-                                <div className="gameCell">9</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <GameBoard />
             </section>
         )
     }
