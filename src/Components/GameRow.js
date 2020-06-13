46ae17b0429330b0932f692ad81bd0c562867d71
dean <deanlloyd64@gmail.com>
2020-06-13 12:22:20 -0400

    create changBoard callback function to feed values back into state after change

--- src/Components/GameRow.js
+++ src/Components/GameRow.js
@@ -6,53 +6,53 @@ export const GameRow = (props) => {
             <div className="gameCellBlock">
                 <div className="gameRow">
                     <div className="gameCellRow">
-                        <div className="gameCell">1</div>
-                        <div className="gameCell">2</div>
-                        <div className="gameCell">3</div>
+                        <div className="gameCell" value={props.row[0][0]}></div>
+                        <div className="gameCell" value={props.row[0][1]}></div>
+                        <div className="gameCell" value={props.row[0][2]}></div>
                     </div>
                     <div className="gameCellRow">
-                        <div className="gameCell">4</div>
-                        <div className="gameCell">5</div>
-                        <div className="gameCell">6</div>
+                        <div className="gameCell" value={props.row[0][3]}></div>
+                        <div className="gameCell" value={props.row[0][4]}></div>
+                        <div className="gameCell" value={props.row[0][5]}></div>
                     </div>
                     <div className="gameCellRow">
-                        <div className="gameCell">7</div>
-                        <div className="gameCell">8</div>
-                        <div className="gameCell">9</div>
-                    </div>
-                </div>
-                <div className="gameRow">
-                    <div className="gameCellRow">
-                        <div className="gameCell">1</div>
-                        <div className="gameCell">2</div>
-                        <div className="gameCell">3</div>
-                    </div>
-                    <div className="gameCellRow">
-                        <div className="gameCell">4</div>
-                        <div className="gameCell">5</div>
-                        <div className="gameCell">6</div>
-                    </div>
-                    <div className="gameCellRow">
-                        <div className="gameCell">7</div>
-                        <div className="gameCell">8</div>
-                        <div className="gameCell">9</div>
+                        <div className="gameCell" value={props.row[0][6]}></div>
+                        <div className="gameCell" value={props.row[0][7]}></div>
+                        <div className="gameCell" value={props.row[0][8]}></div>
                     </div>
                 </div>
                 <div className="gameRow">
                     <div className="gameCellRow">
-                        <div className="gameCell">1</div>
-                        <div className="gameCell">2</div>
-                        <div className="gameCell">3</div>
+                        <div className="gameCell" value={props.row[1][0]}></div>
+                        <div className="gameCell" value={props.row[1][1]}></div>
+                        <div className="gameCell" value={props.row[1][2]}></div>
                     </div>
                     <div className="gameCellRow">
-                        <div className="gameCell">4</div>
-                        <div className="gameCell">5</div>
-                        <div className="gameCell">6</div>
+                        <div className="gameCell" value={props.row[1][3]}></div>
+                        <div className="gameCell" value={props.row[1][4]}></div>
+                        <div className="gameCell" value={props.row[1][5]}></div>
                     </div>
                     <div className="gameCellRow">
-                        <div className="gameCell">7</div>
-                        <div className="gameCell">8</div>
-                        <div className="gameCell">9</div>
+                        <div className="gameCell" value={props.row[1][6]}></div>
+                        <div className="gameCell" value={props.row[1][7]}></div>
+                        <div className="gameCell" value={props.row[1][8]}></div>
+                    </div>
+                </div>
+                <div className="gameRow">
+                    <div className="gameCellRow">
+                        <div className="gameCell" value={props.row[2][0]}></div>
+                        <div className="gameCell" value={props.row[2][1]}></div>
+                        <div className="gameCell" value={props.row[2][2]}></div>
+                    </div>
+                    <div className="gameCellRow">
+                        <div className="gameCell" value={props.row[2][3]}></div>
+                        <div className="gameCell" value={props.row[2][4]}></div>
+                        <div className="gameCell" value={props.row[2][5]}></div>
+                    </div>
+                    <div className="gameCellRow">
+                        <div className="gameCell" value={props.row[2][6]}></div>
+                        <div className="gameCell" value={props.row[2][7]}></div>
+                        <div className="gameCell" value={props.row[2][8]}></div>
                     </div>
                 </div>
             </div>
--- src/Containers/gameBoard.js
+++ src/Containers/gameBoard.js
@@ -24,13 +24,20 @@ export default class GameBoard extends Component{
                     ]
         }
     }
+
+    changeBoard = (position, value) => {
+        this.setState({
+           board: this.state.board[position] = value
+        })
+
+    }
     
     render(){
         return(
             <section className="mainBoard">
                 <GameRow row={this.state.board[0]}/>
-                <GameRow row={this.state.board[0]}/>
-                <GameRow row={this.state.board[0]}/>
+                <GameRow row={this.state.board[1]}/>
+                <GameRow row={this.state.board[2]}/>
 
             </section>
         )
