293b66e543bf97b38bc590d7f39bb4fde9b8de71
dean <deanlloyd64@gmail.com>
2020-06-13 10:48:20 -0400

    create board within the state

--- src/Containers/gameBoard.js
+++ src/Containers/gameBoard.js
@@ -5,14 +5,33 @@ export default class GameBoard extends Component{
     constructor(){
         super()
         this.state = {
-            board: []
+            board:  [
+                        [
+                            ['','','','','','','','',''],
+                            ['','','','','','','','',''],
+                            ['','','','','','','','','']
+                        ],
+                        [
+                            ['','','','','','','','',''],
+                            ['','','','','','','','',''],
+                            ['','','','','','','','','']
+                        ],
+                        [
+                            ['','','','','','','','',''],
+                            ['','','','','','','','',''],
+                            ['','','','','','','','','']
+                        ]
+                    ]
         }
     }
     
     render(){
         return(
             <section className="mainBoard">
-                <GameBoard />
+                <GameBoard row={this.state.board[0][0]}/>
+                <GameBoard row={this.state.board[0][1]}/>
+                <GameBoard row={this.state.board[0][2]}/>
+
             </section>
         )
     }
